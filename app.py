33add6d829126a1d80198d7dab73058b58166ca4
William Dolan <b1lld0l8n@gmail.com>
2022-07-21 00:49:16 -0600

    Code modification

--- app.py
+++ app.py
@@ -21,9 +21,8 @@ def make_forecast(series, df):
     title = series + ' demand (thousand bbls_d)'
     
     model = Prophet()
-    model.add_seasonality(name='monthly', period=30.5, fourier_order=5)
     model.fit(prophet_df)
-    future = model.make_future_dataframe(periods=36, include_history=True)
+    future = model.make_future_dataframe(periods=60, include_history=True)
     forecast = model.predict(future)
 
     fig = plot_plotly(model, forecast)
